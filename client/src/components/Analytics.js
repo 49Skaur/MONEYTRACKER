a09c1e3ed5a86665548243e47179173e598dc31c
49Skaur <skaur49@nait.ca>
2025-04-23 12:15:30 -0600

    Analytics Circle Type Complete

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -9,6 +9,11 @@ function Analytics({ transactions }) {
     const totalIncomeTransactionsPercentage = (totalIncomeTransactions.length / totalTransactions) * 100
     const totalExpenseTransactionsPercentage = (totalExpenseTransactions.length / totalTransactions) * 100
 
+    const totalTurnover = transactions.reduce((acc, transaction) => acc + transaction.amount, 0)
+    const totalIncomeTurnover = transactions.filter(transaction => transaction.type === 'income').reduce((acc, transaction) => acc + transaction.amount, 0)
+    const totalExpenseTurnover = transactions.filter(transaction => transaction.type === 'expense').reduce((acc, transaction) => acc + transaction.amount, 0)
+    const totalIncomeTurnoverPercentage = (totalIncomeTurnover / totalTurnover) * 100
+    const totalExpenseTurnoverPercentage = (totalExpenseTurnover / totalTurnover) * 100
     return (
         <div className='analytics'>
             <div className='row'>
@@ -38,6 +43,34 @@ function Analytics({ transactions }) {
                         </div>
                     </div>
                 </div>
+
+                <div className='col-md-4 mt-3'>
+                    <div className='transactions-count'>
+                        <h4>Total Turnover: {totalTurnover}</h4>
+                        <hr />
+                        <h5>Income : {totalIncomeTurnover}</h5>
+                        <h5>Expense : {totalExpenseTurnover}</h5>
+
+                        <div className='progress-bars'>
+                            <Progress
+                                className='mx-5'
+                                strokeColor='green'
+                                type='circle'
+                                percent={totalIncomeTurnoverPercentage.toFixed(0)}
+                                format={percent => `${percent}%`}
+                            />
+
+                            <Progress
+                                strokeColor='red'
+                                type='circle'
+                                percent={totalExpenseTurnoverPercentage.toFixed(0)}
+                                format={percent => `${percent}%`}
+                            />
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
